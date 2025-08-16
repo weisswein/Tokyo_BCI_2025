d6e88e3b4ded12bbbc99bac90ae7c11f99853255
weisswein <archeologistspeshil@gmail.com>
2025-08-16 15:53:37 +0900

    no message

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+public class UdpReceiver : MonoBehaviour
+{
+    private UdpClient udpClient;
+    private Thread receiveThread;
+    public int port = 5005;  // 任意のポート番号を設定
+    //127.0.0.1
+    void Start()
+    {
+        udpClient = new UdpClient(port);
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+        Debug.Log("UDP Receiver started on port " + port);
+    }
+
+    private void ReceiveData()
+    {
+        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
+        while (true)
+        {
+            try
+            {
+                byte[] data = udpClient.Receive(ref remoteEndPoint);
+                string message = Encoding.UTF8.GetString(data);
+                Debug.Log("Received: " + message);
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e.ToString());
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (receiveThread != null) receiveThread.Abort();
+        udpClient.Close();
+    }
+}
